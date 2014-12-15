de9d45d9b205130059fb3519502c017ee755eda9
Kari K Korpinen <kkkorpin@sf-8.cs.helsinki.fi>
2014-12-15 14:03:59 +0200

    first

---
+++ count.java
@@ -0,0 +1,16 @@
+public class count{
+
+	public count(){
+
+	}
+	public void print()
+	{
+		System.out.println("Count testing");
+	}
+	
+
+public static void main(String[] args) {
+	count kaksi=new count();
+	kaksi.print();
+   }
+}
